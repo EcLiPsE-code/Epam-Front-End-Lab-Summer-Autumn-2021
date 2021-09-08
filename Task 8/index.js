1efa95ced7df2200bdfd279f9a8dec072083a9d6
Solodkov Mikhail <fantazermyname@gmail.com>
2021-09-09 02:56:22 +0300

    task complete (version 1.0.1)

--- Task 8/index.js
+++ Task 8/index.js
@@ -1,13 +1,10 @@
-const result = []
+const test = [1, 2, 3, 4, 5]
+const test2 = [[1, 3], [4, 5, 6], [7, 8, 9]]
 
-function multidimensionalOperation(array){
-    return array.reduce((prevValue, value) => {
-        Array.isArray(value)? multidimensionalOperation(value) : prevValue.push(value)
-        return prevValue
-    }, result).map((value, index, arr) => {
+function oneDimensionalOperation(arr){
+    return arr.map((value, index, array) => {
         return index === 0? arr[index + 1]
             : index === arr.length - 1? arr[index - 1] : arr[index - 1] + arr[index + 1]
     })
 }
-
-console.log(multidimensionalOperation([1, 2]))
+console.log(oneDimensionalOperation(test))
