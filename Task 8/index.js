ff31f8d0a39567086e9a2a6fe025f0dbb32976bf
Solodkov Mikhail <fantazermyname@gmail.com>
2021-09-09 19:45:25 +0300

    task complete (version 1.0.2)

--- Task 8/index.js
+++ Task 8/index.js
@@ -1,10 +1,10 @@
 const test = [1, 2, 3, 4, 5]
-const test2 = [[1, 3], [4, 5, 6], [7, 8, 9]]
 
 function oneDimensionalOperation(arr){
     return arr.map((value, index, array) => {
         return index === 0? arr[index + 1]
-            : index === arr.length - 1? arr[index - 1] : arr[index - 1] + arr[index + 1]
+            : index === arr.length - 1? array[index - 1] : array[index - 1] + array[index + 1]
     })
 }
+
 console.log(oneDimensionalOperation(test))
