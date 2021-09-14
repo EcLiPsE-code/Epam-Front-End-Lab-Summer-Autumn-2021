d301c8695e136f26bc50ff614776e5dcacb037fe
Solodkov Mikhail <fantazermyname@gmail.com>
2021-09-15 01:15:29 +0300

    project initialize

---
+++ Task on CSS and HTML (Blog)/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HTMLWebpackPlugin()
+    ]
+}
