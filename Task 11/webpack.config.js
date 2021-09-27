348ad4ce70c6d8091530683f7886f76b5358ffa9
Solodkov Mikhail <fantazermyname@gmail.com>
2021-09-27 22:14:00 +0300

    project initialize

---
+++ Task 11/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 4000
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+}
